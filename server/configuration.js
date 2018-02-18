63086e2df6a9788781e54467541a1e8405b4825a
Kelly Copley <copleykj@gmail.com>
2018-02-17 21:29:32 -0500

    rework auth rules

--- server/configuration.js
+++ server/configuration.js
@@ -1,6 +1,16 @@
 /* eslint-disable import/no-unresolved */
+import { Meteor } from 'meteor/meteor';
 import Cloudinary from 'cloudinary';
 
-Cloudinary.rules = {};
+const defaultAuth = function defaultAuth() {
+    throw new Meteor.Error('Unauthorized', 'You are not authorized to perform this action');
+};
+
+Cloudinary.rules = {
+    delete: defaultAuth,
+    sign_upload: defaultAuth,
+    private_resource: defaultAuth,
+    download_url: defaultAuth,
+};
 
 export { Cloudinary };
--- server/server.js
+++ server/server.js
@@ -3,53 +3,34 @@ import { Meteor } from 'meteor/meteor';
 import { check, Match } from 'meteor/check';
 import Future from 'fibers/future';
 /* eslint-enable import/no-unresolved */
-
+import './versionCheck.js';
 import { Cloudinary } from './configuration.js';
 
 
 Meteor.methods({
     'cloudinary.sign': function sign(ops = {}) {
-        let authFunction;
-
         check(ops, Match.Optional(Object));
         this.unblock();
-        if (Cloudinary.rules.sign_upload) {
-            this.options = ops;
-            authFunction = Cloudinary.rules.signature.bind(this);
-            if (!authFunction()) {
-                throw new Meteor.Error('Unauthorized', 'Signature not allowed');
-            }
-        }
-        const signature = Cloudinary.uploader.direct_upload('', ops);
-        return signature;
+        Cloudinary.rules.sign_upload.call(this);
+
+        return Cloudinary.uploader.direct_upload('', ops);
     },
     'cloudinary.delete_by_publicId': function deleteByPublicId(publicId, type) {
-        let authFunction;
         let ops;
         check(publicId, String);
         check(type, Match.OneOf(String, 0, null));
         this.unblock();
-        if (Cloudinary.rules.delete) {
-            this.publicId = publicId;
-            authFunction = Cloudinary.rules.delete.bind(this);
-            if (!authFunction()) {
-                throw new Meteor.Error('Unauthorized', 'Delete not allowed');
-            }
-        }
+        Cloudinary.rules.delete.call(this, publicId);
         if (type) {
             ops = {
                 type,
             };
         }
         const future = new Future();
-
         Cloudinary.api.delete_resources([publicId], result => future.return(result), ops);
-
         return future.wait();
     },
     'cloudinary.get_private_resource': function getPrivateResource(publicId, ops = {}) {
-        let authFunction;
-
         check(publicId, String);
         check(ops, Match.Optional(Object));
         this.unblock();
@@ -58,28 +39,14 @@ Meteor.methods({
             sign_url: true,
             type: 'private',
         };
-        if (Cloudinary.rules.private_resource) {
-            this.publicId = publicId;
-            authFunction = Cloudinary.rules.private_resource.bind(this);
-            if (!authFunction()) {
-                throw new Meteor.Error('Unauthorized', 'Access not allowed');
-            }
-        }
+        Cloudinary.rules.private_resource.call(this, publicId);
         return Cloudinary.url(publicId, newOps);
     },
     'cloudinary.get_download_url': function getDownloadUrl(publicId, ops = {}) {
-        let authFunction;
-
         check(publicId, String);
         check(ops, Match.Optional(Object));
         this.unblock();
-        if (Cloudinary.rules.download_url) {
-            this.publicId = publicId;
-            authFunction = Cloudinary.rules.download_url.bind(this);
-            if (!authFunction()) {
-                throw new Meteor.Error('Unauthorized', 'Access not allowed');
-            }
-        }
+        Cloudinary.rules.download_url.call(this, publicId);
         const format = ops.format || '';
         return Cloudinary.utils.private_download_url(publicId, format, ops.filter(op => op !== 'format'));
     },
