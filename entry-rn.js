655cc38e5dd89842452c8e3c7dc43d35bb425600
Kelly Copley <copleykj@gmail.com>
2018-11-18 14:26:07 -0500

    fix RN imports

--- entry-rn.js
+++ entry-rn.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved, no-param-reassign */
 import Meteor, { Mongo } from '@socialize/react-native-meteor';
 
-import CloudinaryConstruct from './cloudinary.js';
+import CloudinaryConstruct from './client/cloudinary.js';
 
 const Cloudinary = CloudinaryConstruct({ Meteor, Mongo, Promise });
 
