50d03c6705cda64c5bcb1f146890b94b64153fe3
Grant Hobbs <grant@gwhobbs.com>
2020-06-13 20:11:27 -0500

    Bump npm package versions

--- client/versionCheck.js
+++ client/versionCheck.js
@@ -2,5 +2,5 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
-    'cloudinary-core': '2.6.x',
+    'cloudinary-core': '2.8.x',
 }, 'socialize:cloudinary');
--- server/versionCheck.js
+++ server/versionCheck.js
@@ -2,5 +2,5 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
-    cloudinary: '1.14.x',
+    cloudinary: '1.22.x',
 }, 'socialize:cloudinary');
